0f59b51ae3374c391504cea3dcd05a4ac586d246
Colin Bares <colinedwinbares@gmail.com>
2026-03-09 02:47:09 -0500

    Create card and roll button

--- src/app/cbares-dice-roll/cbares-dice-roll.ts
+++ src/app/cbares-dice-roll/cbares-dice-roll.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-cbares-dice-roll',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './cbares-dice-roll.html',
   styleUrl: './cbares-dice-roll.css',
 })
 export class CbaresDiceRoll {
-
+  protected readonly rollDice = () => {
+    console.log('roll button clicked');
+  }
 }
