7aff8181e0808f52450b280edb795b0f5b3d814a
THamilton1123 <thamilton12@madisoncollege.edu>
2026-03-11 11:34:13 -0500

    Placeholders there...
    Now figure out MD inputs and button...
    
    Then make work...
    
    Included d20 button, commented out to continue coding along with video without error message...

--- src/app/thamilton-roll-d20/thamilton-roll-d20.ts
+++ src/app/thamilton-roll-d20/thamilton-roll-d20.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
   selector: 'app-thamilton-roll-d20',
-  imports: [],
+  imports: [MatCardModule, MatAnchor],
   templateUrl: './thamilton-roll-d20.html',
   styleUrl: './thamilton-roll-d20.css',
 })
 export class ThamiltonRollD20 {
-
+  private readonly rollD20 = () => {};
 }
