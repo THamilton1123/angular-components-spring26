31045b43a1e3eae34170c7605161a67bdd2f6936
Zac-Smucker-Bryan <zacary.bryan@gmail.com>
2026-03-16 08:44:56 -0500

    Setting Up Checkbox styles
    + Fixing Sausage typo

--- src/app/zsmuckerbryan-checklist/zsmuckerbryan-checklist.ts
+++ src/app/zsmuckerbryan-checklist/zsmuckerbryan-checklist.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-zsmuckerbryan-checklist',
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatCheckboxModule
   ],
   templateUrl: './zsmuckerbryan-checklist.html',
   styleUrl: './zsmuckerbryan-checklist.css',
