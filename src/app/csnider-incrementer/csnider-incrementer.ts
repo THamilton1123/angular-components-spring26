33b47fd60565cdec4336e0e58d08bda544d95478
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-04-07 15:29:45 +0000

    Removed Material buttons. Created and styled traditional HTML buttons

--- src/app/csnider-incrementer/csnider-incrementer.ts
+++ src/app/csnider-incrementer/csnider-incrementer.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-csnider-incrementer',
-  imports: [MatCardModule, MatButtonModule, MatIconModule],
+  imports: [MatCardModule, MatIconModule],
   templateUrl: './csnider-incrementer.html',
   styleUrl: './csnider-incrementer.css',
 })
