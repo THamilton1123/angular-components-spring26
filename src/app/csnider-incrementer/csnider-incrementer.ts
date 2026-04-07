693716b9458c2e607c5229d2683687c985e9d832
Craig Snider <36526783+cwsnider@users.noreply.github.com>
2026-04-07 14:24:10 +0000

    Added control buttons to Incrementer

--- src/app/csnider-incrementer/csnider-incrementer.ts
+++ src/app/csnider-incrementer/csnider-incrementer.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-csnider-incrementer',
-  imports: [MatCardModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './csnider-incrementer.html',
   styleUrl: './csnider-incrementer.css',
 })
